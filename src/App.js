da44d773626f66da2f959d332be13dc0cc469c69
JoelAngels <joelbaraka4325@gmail.com>
2021-03-03 19:02:01 +0300

    last scene of the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+//let us now access the counter
+
+import { useSelector } from "react-redux";
 function App() {
+  const counter = useSelector((state) => state.counter); //I can have the whole counter state here
+  const isLogged = useSelector((state) => state.isLogged);
   return (
     <div className="App">
-      <h1>Hello Chapati!</h1>
+      <h1>Lets go Redux {counter}</h1>
+      {/* Let us now increase the counter */}
+      <button>+</button>
+      <button>-</button>
+      {/* it is set to false so
+      nothing will show up */}
+      {isLogged ? <h3>Valuable Info unseen </h3> : ""}
     </div>
   );
 }
